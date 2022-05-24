2e837ad9881656116c1ce57b931b983a07ec819a
Amir <amirhatam136@gmail.com>
2022-05-24 16:36:52 +0200

    add Main Page, import axios, Get data from API

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,24 @@ import axios from 'axios'
 
 
 export default function Main() {
+    const [cats, setCats] = useState([])
 
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get("https://catfact.ninja/breeds?page=1")
+                const data = response.data.data
+                // console.log(data);
+                if (response.status === 200) {
+                    setCats(data)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        })();
+    }, [])
+
+    console.log(cats);
 
     return (
         <div>
